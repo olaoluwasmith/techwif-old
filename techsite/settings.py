79c718675f8a7bad63420ff01d1de427abf4d0f9
Smith <olaoluwasamsmith@gmail.com>
2020-09-05 19:01:33 +0100

    heroku deployment

--- techsite/settings.py
+++ techsite/settings.py
@@ -121,14 +121,14 @@ WSGI_APPLICATION = 'techsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-#        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'tech_1',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'olaoluwasmith',
-        'PASSWORD': 'smithpython12',
-        'HOST': 'database-1.ccshgogewdou.us-west-2.rds.amazonaws.com',
-        'PORT': '5432'
+#        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'tech_1',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'USER': 'olaoluwasmith',
+#        'PASSWORD': 'smithpython12',
+#        'HOST': 'database-1.ccshgogewdou.us-west-2.rds.amazonaws.com',
+#        'PORT': '5432'
     }
 }
 
@@ -169,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
