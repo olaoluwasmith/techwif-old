85628023aa5cd552499fd73f53a16cd8bb35f35d
Smith <olaoluwasamsmith@gmail.com>
2020-09-05 18:48:49 +0100

    heroku deployment

--- techsite/settings.py
+++ techsite/settings.py
@@ -170,10 +170,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
-STATIC_URL = 'static'
+STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'static/media'
+MEDIA_URL = 'static/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
