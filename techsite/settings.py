be3b52c07a502a04206999c2da1e35c08a2f60f5
Smith <olaoluwasamsmith@gmail.com>
2020-09-06 17:09:12 +0100

    heroku postgres

--- techsite/settings.py
+++ techsite/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/static/media/'
