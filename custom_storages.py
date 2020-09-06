4c460c0dfe269faa91072641307d5568a60a9180
Smith <olaoluwasamsmith@gmail.com>
2020-09-06 16:06:03 +0100

    AWS

--- custom_storages.py
+++ custom_storages.py
@@ -1,4 +1,4 @@
-"""from django.conf import settings
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -6,5 +6,5 @@ class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION """
+#class MediaStorage(S3Boto3Storage):
+#    location = settings.MEDIAFILES_LOCATION
