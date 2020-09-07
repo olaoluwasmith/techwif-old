dafb6b8c212a228591f80065ac80ae4e21d5a408
Smith <olaoluwasamsmith@gmail.com>
2020-09-07 19:52:05 +0100

    uninstall whitenoise

--- techsite/settings.py
+++ techsite/settings.py
@@ -226,7 +226,7 @@ AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-STATIC_LOCATION = 'static'
+STATIC_LOCATION = 'staticfiles'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
