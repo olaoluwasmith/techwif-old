2f93fcabf17896631463bfe68dc913e817240092
Smith <olaoluwasamsmith@gmail.com>
2020-09-08 21:42:16 +0100

    debug

--- techsite/settings.py
+++ techsite/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '!fpr1f8%47-auv*+$t053on)ozn#7i&6t4vkjo+(836^=t38kn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -176,7 +176,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 LOGIN_URL = 'login'
 
@@ -216,7 +216,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media/')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
