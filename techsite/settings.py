8dc4e780b8f8ffdfffa81978ab47f9f64e75952d
Smith <olaoluwasamsmith@gmail.com>
2020-09-18 13:28:14 +0100

    smtp done

--- techsite/settings.py
+++ techsite/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '!fpr1f8%47-auv*+$t053on)ozn#7i&6t4vkjo+(836^=t38kn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.techwif.com', '127.0.0.1']
 
@@ -186,3 +186,15 @@ AUTH_USER_MODEL = 'user_unique_email.User'
 
 DJANGO_NOTIFICATIONS_CONFIG = { 'USE_JSONFIELD': True }
 DJANGO_NOTIFICATIONS_CONFIG = { 'SOFT_DELETE': True }
+
+"""
+# S3 BUCKET SET UP
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = 'techwif'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+"""
