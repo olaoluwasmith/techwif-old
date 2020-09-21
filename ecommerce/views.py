c980c03c7a50153c3c0c9de81e6b18da0a9dfba9
Smith <olaoluwasamsmith@gmail.com>
2020-09-21 22:56:53 +0100

    password reset domain

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -110,6 +110,7 @@ def processOrder(request):
             city=data['shipping']['city'],
             state=data['shipping']['state'],
             zipcode=data['shipping']['zipcode'],
+            country=data['shipping']['country'],
         )
 
     return JsonResponse('Payment complete!', safe=False)
