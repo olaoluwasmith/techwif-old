aa2e39855dcf8203a4f2935805fb0ed9dcd232ae
Smith <olaoluwasamsmith@gmail.com>
2020-09-22 13:04:52 +0100

    reset form

--- tech/urls.py
+++ tech/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
         auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_sent.html'), 
         name='password_reset_done'),
     path('reset/<uidb64>/<token>/', 
-        auth_views.PasswordResetConfirmView.as_view(template_name='user/password_form.html'), 
+        auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_form.html'), 
         name='password_reset_confirm'),
     path('reset_password_complete/', 
         auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), 
