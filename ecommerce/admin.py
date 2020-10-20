c0d710120f473ecaa576197802fc77c7b5dd20d3
Smith <olaoluwasamsmith@gmail.com>
2020-10-20 11:20:05 +0100

    ecommerce admin

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -12,9 +12,21 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description', 'price', 'category']
     prepopulated_fields = {'slug': ('name', )}
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'date_ordered', 'complete', 'transaction_id')
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'order', 'quantity', 'date_added')
+
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'order', 'address', 'city', 'state', 'zipcode', 'country', 'date_added')
+
 admin.site.register(Customer)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
 admin.site.register(ProductCategory, ProductCategoryAdmin)
