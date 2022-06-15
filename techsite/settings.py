197d3e42c0013475e48f6f59540b019866eb1515
Smith <olaoluwasamsmith@gmail.com>
2022-06-15 13:49:11 +0100

    article-detail

--- techsite/settings.py
+++ techsite/settings.py
@@ -27,7 +27,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.techwif.com', '127.0.0.1', 'techwif.herokuapp.com']
 
